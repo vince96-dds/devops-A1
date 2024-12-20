ea51fbd88836fdc4f9ae0739eb4f1c1729a84338
valfr <valfr@host.docker.internal>
2024-12-20 12:31:21 -0600

    Exponential function is added

--- src/main/java/calcpkg/MyCalc.java
+++ src/main/java/calcpkg/MyCalc.java
@@ -5,7 +5,11 @@ package calcpkg;
 public class MyCalc {
 	
 	public int sum(int a,int b) {
-		return a+b;
+		int c;
+		int d;
+		c=10+5;
+		return c;
+		//return a+b;
 	}
 	public int diff(int a,int b) {
 		return a-b;
@@ -15,6 +19,30 @@ public class MyCalc {
 	public int Mult(int a,int b) {
 		return a*b;
 	}
+	
+	public int Div(int a,int b) {
+		return a/b;
+	}
+	public int Exponential(int variable, int exponent){
+		
+		int num=variable;
+		for(int i=0; i<exponent;i++) {
+			num*=num;
+		}
+		return num;
+	}
+	
+//	public int Addsubtract(int a, int b) {
+//		int num=a;
+//		num+=5;
+//		num-=b;
+//		num*=8;
+//		return num;
+//		
+//	}
+	public void greeting() {
+		System.out.println("Hello Jenkins");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -22,6 +50,9 @@ public class MyCalc {
 		System.out.println("Sum is:"+pb.sum(20,10));
 		System.out.println("Diff is:"+pb.diff(20,10));
 		System.out.println("Mult is:"+pb.Mult(20,10));
+		System.out.println("Div is:"+pb.Div(20,10));
+		System.out.println("Exponent is: "+pb.Exponential(20, 10))
+		pb.greeting();
 	}
 
 }
